459607d1a5dc286a88c6ac3ab0e6f76d55767a12
isnendyankp <isnendyankp@gmail.com>
2023-07-06 15:59:21 +0700

    S9-57: add new define constuctor

--- src/User.ts
+++ src/User.ts
@@ -4,6 +4,10 @@ class User {
     lat: number;
     lng: number;
   };
+
+  constructor() {}
 }
 
 // create 2 property: name & location
+// this case want to randomly generate name, latitude and longtitude 
+// initialization inside of constructor function instead
