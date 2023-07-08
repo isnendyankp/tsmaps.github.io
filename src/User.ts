a1a4248840eca73e12844259e1982129000a1a15
isnendyankp <isnendyankp@gmail.com>
2023-07-08 13:05:45 +0700

    S9-59:using parseFloat for convert string to number

--- src/User.ts
+++ src/User.ts
@@ -9,18 +9,20 @@ class User {
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
-      lat:
-      lng:
-    }
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
   }
 }
 
-// add progression :
+// Step progression :
 // - create 2 property: name & location
 // - this case want to randomly generate name, latitude and longtitude 
 // - initialization inside of constructor function instead
 // - import faker module for generating random data
 // - initialization name in constructor = this.name = faker.name.firstName();
-// - randomly generrate a name and assign it to that name property
+// - randomly generate a name and assign it to that name property
 // - initialization location property to be an object in constructor = this.location = {}
 // - lat & lng = property object in constructor
+// - randomly generate a latitude & longtitude with faker = lat: faker.address.latitude(), lng: faker.address.longtitude()
+// - using parseFloat for convert string to number (lat & lng default type is number)
