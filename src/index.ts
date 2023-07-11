fba16b61c603843a539c01f24244c15f6ea70b1d
isnendyankp <isnendyankp@gmail.com>
2023-07-11 16:35:18 +0700

    S9-60: cr8 equal const user with check

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { User } from './User';
 
+const user = new User();
 
+console.log(user);
 
 
 // step progress:
 // - import class user from User.ts
+// - cr8 equal const user with checking console.log(user);
