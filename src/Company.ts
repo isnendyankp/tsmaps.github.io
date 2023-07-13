096d7d30ea42a63f27439aca77d1c3d23057cf5d
isnendyankp <isnendyankp@gmail.com>
2023-07-13 16:18:17 +0700

    S9-61: add initialize property companyPhrase

--- src/Company.ts
+++ src/Company.ts
@@ -10,13 +10,14 @@ export class Company {
 
   constructor() {
     this.companyName = faker.company.name();
-    
+    this.catchPhrase = faker.company.catchPhrase();
     };
 }
 
 
-// step progress:
-// cr8 file with import & cr8 base for export class Company 
-//  cr8 3 property: companyName, catchPhrase and location with type
-//  define constructor function for initialize all properties
-//  initialize property companyName  for value from faker.company.name() which is companyName came from randomly generated using faker library. 
+// Step progress:
+// - cr8 file with import & cr8 base for export class Company 
+// - cr8 3 property: companyName, catchPhrase and location with type
+// - define constructor function for initialize all properties
+// - initialize property companyName  for value from faker.company.name() which is companyName came from randomly generated using faker library. 
+// - initialize property companyPhrase fo generate catchy phrases or slogans which are also generated randomly using the faker library and are generally used to represent a company in a nutshell.
