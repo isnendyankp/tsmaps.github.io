509d43e702f23b784ed4bd83d770193cd0e4ec14
isnendyankp <isnendyankp@gmail.com>
2023-07-13 16:25:26 +0700

    S9-61: import class Compnay & cr8 variable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
 
 console.log(user);
 
+const company = new Company();
+
+console.log(company);
 
 // step progress:
 // - import class user from User.ts
-// - cr8 equal const user with checking console.log(user);
+// - cr8 variable equal class User with checking console.log(user);
+// - import class Company from Company.ts
+// -  cr8 variable equal class Company with checking console.log(company);
