9e343e3a547a8aa19c97681a7eb01e20e35a2569
isnendyankp <isnendyankp@gmail.com>
2023-07-13 16:14:58 +0700

    S9-61: add initialize property companyName

--- src/Company.ts
+++ src/Company.ts
@@ -7,9 +7,16 @@ export class Company {
     lat: number;
     lng: number;
   };
+
+  constructor() {
+    this.companyName = faker.company.name();
+    
+    };
 }
 
 
 // step progress:
 // cr8 file with import & cr8 base for export class Company 
 //  cr8 3 property: companyName, catchPhrase and location with type
+//  define constructor function for initialize all properties
+//  initialize property companyName  for value from faker.company.name() which is companyName came from randomly generated using faker library. 
