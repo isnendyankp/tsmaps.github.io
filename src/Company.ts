01a33b7aa0aefc40432ddc6050af214a87bdd3fc
isnendyankp <isnendyankp@gmail.com>
2023-07-13 16:20:50 +0700

    S9-61: initialize property location

--- src/Company.ts
+++ src/Company.ts
@@ -11,7 +11,11 @@ export class Company {
   constructor() {
     this.companyName = faker.company.name();
     this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
     };
+  }
 }
 
 
@@ -20,4 +24,5 @@ export class Company {
 // - cr8 3 property: companyName, catchPhrase and location with type
 // - define constructor function for initialize all properties
 // - initialize property companyName  for value from faker.company.name() which is companyName came from randomly generated using faker library. 
-// - initialize property companyPhrase fo generate catchy phrases or slogans which are also generated randomly using the faker library and are generally used to represent a company in a nutshell.
+// - initialize property companyPhrase for generate catchy phrases or slogans which are also generated randomly using the faker library and are generally used to represent a company in a nutshell.
+// - initialize property location for generates a location or place with latitude and longitude coordinates taken randomly using faker.
