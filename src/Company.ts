2ba84ad1ad4636f7a7e021409288db23ef6cd499
isnendyankp <isnendyankp@gmail.com>
2023-07-13 16:07:43 +0700

    S9-61: cr8 3 property on Company

--- src/Company.ts
+++ src/Company.ts
@@ -1,9 +1,15 @@
 import faker from 'faker';
 
 export class Company {
-
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 }
 
 
 // step progress:
 // cr8 file with import & cr8 base for export class Company 
+//  cr8 3 property: companyName, catchPhrase and location with type
