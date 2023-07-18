d9be91b7d88c38ac08ac93c79065bf13f631643b
isnendyankp <isnendyankp@gmail.com>
2023-07-18 16:44:52 +0700

    S9-68: add property googleMap with instance class

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,7 +1,8 @@
 export class CustomMap {
-
+  googleMap: google.maps.Map;
 }
 
 
 // step Progress:
 // - cr8 base class CustomMap & export it
+// - add property googleMap with instance of the class Google Maps map
