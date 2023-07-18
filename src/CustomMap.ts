9978f1b12128054a16a7b2ab4488136eceab1398
isnendyankp <isnendyankp@gmail.com>
2023-07-18 16:48:02 +0700

    S9-68: cr8 base constructor

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,13 @@
 export class CustomMap {
   googleMap: google.maps.Map;
+
+  constructor() {
+
+  }
 }
 
 
 // step Progress:
-// - cr8 base class CustomMap & export it
-// - add property googleMap with instance of the class Google Maps map
+// - cr8 base class CustomMap & export it: export class CustomMap {}
+// - add property googleMap with instance of the class Google Maps map: googleMap: google.maps.Map;
+// - create constructor for initialize & cr8 this map and show it on the screen: constructor() {}
