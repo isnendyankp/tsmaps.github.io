ccd453bb4bd1f4158a8d060f654c37486804abc9
isnendyankp <isnendyankp@gmail.com>
2023-07-19 15:56:30 +0700

    S9-68: add initialize property googleMap

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -2,7 +2,7 @@ export class CustomMap {
   googleMap: google.maps.Map;
 
   constructor() {
-
+    this.googleMap =
   }
 }
 
@@ -11,3 +11,4 @@ export class CustomMap {
 // - cr8 base class CustomMap & export it: export class CustomMap {}
 // - add property googleMap with instance of the class Google Maps map: googleMap: google.maps.Map;
 // - create constructor for initialize & cr8 this map and show it on the screen: constructor() {}
+// - add initialize property googleMap in constructor
